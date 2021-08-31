ce520f88f209c63628378656019c3d914de75ed4
Aleksandr <msg2fedorov@gmail.com>
2021-08-31 11:19:08 +0300

    first

---
+++ src/main/java/staffcontrol_lite/staffcontrol_lite/StaffcontrolLiteApplication.java
@@ -0,0 +1,13 @@
+package staffcontrol_lite.staffcontrol_lite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffcontrolLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffcontrolLiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/staffcontrol_lite/staffcontrol_lite/StaffcontrolLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package staffcontrol_lite.staffcontrol_lite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffcontrolLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
