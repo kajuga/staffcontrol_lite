55be161684d7fbec6189f2adda4c4126883d6dec
Aleksandr <msg2fedorov@gmail.com>
2021-09-05 14:54:54 +0300

    added rest

---
+++ src/main/java/com/staffcontrol_lite/rest/PersonRestControllerV1.java
@@ -0,0 +1,13 @@
+package com.staffcontrol_lite.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/person")
+public class PersonRestControllerV1 {
+
+
+
+
+}
